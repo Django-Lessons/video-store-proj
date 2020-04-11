598c91aac7b3fd220322913c63ac31d595344e1a
Eugen Ciur <eugen@django-lessons.com>
2020-04-11 12:16:36 +0200

    basic app

--- land/urls.py
+++ land/urls.py
@@ -4,4 +4,6 @@ from land import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
 ]
