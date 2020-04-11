eec4196ab52106d0d0a2e5ccd7810ea31d7e8765
Eugen Ciur <eugen@django-lessons.com>
2020-04-11 12:05:43 +0200

    minimal video store application

--- land/views.py
+++ land/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'land/index.html')
+
+
+def about(request):
+    return render(request, 'land/about.html')
+
+
+def contact(request):
+    return render(request, 'land/contact.html')
+ 
