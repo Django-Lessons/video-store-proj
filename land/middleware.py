abfb0c3edcfec00371eff079c78ae05cb70c2902
Eugen Ciur <eugen@django-lessons.com>
2020-04-12 16:29:20 +0200

    for demonstration purpose, I included one more middleware class

--- land/middleware.py
+++ land/middleware.py
@@ -4,17 +4,34 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class RequestLogger:
+class RequestLogger1:
 
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
 
+        logger.info("BEFORE RL1")
+
         response = self.get_response(request)
 
-        logger.info(
-            f"{request.method} path={request.path}"
-        )
+        logger.info("AFTER RL1")
+
+        return response
+
+
+class RequestLogger2:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        logger.info("BEFORE RL2")
+
+        response = self.get_response(request)
+
+        logger.info("AFTER RL2")
+        logger.info(f"method={request.method} path={request.path}")
 
         return response
--- videostoreproj/settings/base.py
+++ videostoreproj/settings/base.py
@@ -42,14 +42,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'land.middleware.RequestLogger1',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'land.middleware.RequestLogger2',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'land.middleware.RequestLogger'
 ]
 
 ROOT_URLCONF = 'videostoreproj.urls'
