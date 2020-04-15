515ef33ec9f4a31085ae00f725f56c1583004a48
Eugen Ciur <eugen@django-lessons.com>
2020-04-15 07:20:07 +0200

    video storage looks really good with cards

--- land/urls.py
+++ land/urls.py
@@ -1,9 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from land import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('video', views.video, name='video'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
--- land/views.py
+++ land/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from land.models import Video
 
 
 def index(request):
-    return render(request, 'land/index.html')
+    videos = Video.objects.all()
+    return render(
+        request,
+        'land/index.html',
+        {'videos': videos}
+    )
+
+
+def video(request, id):
+    video = Video.find(id=id)
+
+    return render(
+        request,
+        'land/index.html',
+        {'video': video}
+    )
 
 
 def about(request):
