66ac384823cc806186b6c655f2553da6cb9827dd
Eugen Ciur <eugen@django-lessons.com>
2020-04-28 08:20:00 +0200

    stripe payments

--- land/views.py
+++ land/views.py
@@ -1,9 +1,13 @@
+import stripe
 import logging
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 from django.shortcuts import render
+from django.conf import settings
+from land.payments import VideosPlan
 from land.models import Video
 
-
+API_KEY = settings.STRIPE_SECRET_KEY
 logger = logging.getLogger(__name__)
 
 
@@ -47,9 +51,32 @@ def upgrade(request):
     return render(request, 'land/payments/upgrade.html')
 
 
+@require_POST
 @login_required
 def payment_method(request):
-    pass
+    stripe.api_key = API_KEY
+    plan = request.POST.get('plan', 'm')
+    automatic = request.POST.get('automatic', 'on')
+    payment_method = request.POST.get('payment_method', 'card')
+    context = {}
+
+    plan_inst = VideosPlan(plan_id=plan)
+    #               plan_inst.amount
+    # 'a' | 'm' =>  plan_inst.currency
+    #               plan_inst.stripe_plan_id
+
+    payment_intent = stripe.PaymentIntent.create(
+        amount=plan_inst.amount,
+        currency=plan_inst.currency,
+        payment_method_types=['card']
+    )
+
+    if payment_method == 'card':
+
+        context['secret_key'] = payment_intent.client_secret
+        context['STRIPE_PUBLISHABLE_KEY'] = settings.STRIPE_PUBLISHABLE_KEY
+
+        return render(request, 'land/payments/card.html', context)
 
 
 @login_required
