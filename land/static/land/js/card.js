da5a09b56f94a9ca6e0a154f3aa974885af0e583
Eugen Ciur <eugen@django-lessons.com>
2020-04-28 09:14:42 +0200

    js part

--- land/static/land/js/card.js
+++ land/static/land/js/card.js
@@ -1,5 +1,26 @@
 function card(stripe_publishable_key, customer_email) {
     document.addEventListener("DOMContentLoaded", function(event){
+        var stripe = Stripe(stripe_publishable_key);
+        var elements = stripe.elements();
 
+        var style = {
+          base: {
+            color: '#32325d',
+            fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+            fontSmoothing: 'antialiased',
+            fontSize: '16px',
+            '::placeholder': {
+              color: '#aab7c4'
+            }
+          },
+          invalid: {
+            color: '#fa755a',
+            iconColor: '#fa755a'
+          }
+        };
+
+        // Create an instance of the card Element.
+        var card = elements.create('card', {style: style});
+        card.mount("#card-element");
     }); // DOMContentLoaded
 }
