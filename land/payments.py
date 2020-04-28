be2a6111c1672ba105b5415f32ce861e9a0a3b81
Eugen Ciur <eugen@django-lessons.com>
2020-04-28 09:26:49 +0200

    add payments.py to land app

---
+++ land/payments.py
@@ -0,0 +1,47 @@
+import logging
+
+from django.conf import settings
+
+MONTH = 'm'
+ANNUAL = 'a'
+
+API_KEY = settings.STRIPE_SECRET_KEY
+logger = logging.getLogger(__name__)
+
+
+class VideosMonthPlan:
+    def __init__(self):
+        self.stripe_plan_id = settings.STRIPE_PLAN_MONTHLY_ID
+        self.amount = 1995
+
+
+class VideosAnnualPlan:
+    def __init__(self):
+        self.stripe_plan_id = settings.STRIPE_PLAN_ANNUAL_ID
+        self.amount = 19950
+
+
+class VideosPlan:
+    def __init__(self, plan_id):
+        """
+        plan_id is either string 'm' (stands for monthly)
+        or a string letter 'a' (which stands for annual)
+        """
+        if plan_id == MONTH:
+            self.plan = VideosMonthPlan()
+            self.id = MONTH
+        elif plan_id == ANNUAL:
+            self.plan = VideosAnnualPlan()
+            self.id = ANNUAL
+        else:
+            raise ValueError('Invalid plan_id value')
+
+        self.currency = 'usd'
+
+    @property
+    def stripe_plan_id(self):
+        return self.plan.stripe_plan_id
+
+    @property
+    def amount(self):
+        return self.plan.amount
