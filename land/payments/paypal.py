ec97158fde934a73b497c35bb4fb6e0e12206c05
Eugen Ciur <eugen@django-lessons.com>
2020-05-06 09:13:24 +0200

    refactoring paypal integration

---
+++ land/payments/paypal.py
@@ -0,0 +1,95 @@
+import os
+import yaml
+import paypalrestsdk
+import logging
+from land.models import User
+
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+
+BASE_DIR = os.path.join(
+    "..",  # proj
+    "..",  # land
+    os.path.dirname(__file__)  # commands
+)
+
+PRODUCT_CONF_PATH = os.path.join("paypal", "product.yml")
+PLAN_CONF_PATH = os.path.join("paypal", "plan.yml")
+ORDER_CONF_PATH = os.path.join("paypal", "order.yml")
+
+
+def mode():
+    if settings.DEBUG:
+        return "sandbox"
+
+    return "live"
+
+
+myapi = paypalrestsdk.Api({
+    "mode": mode(),  # noqa
+    "client_id": settings.PAYPAL_CLIENT_ID,
+    "client_secret": settings.PAYPAL_CLIENT_SECRET
+})
+
+
+def get_plan():
+    data = None
+
+    with open(PLAN_CONF_PATH, "r") as f:
+        data = yaml.safe_load(f)
+        logger.debug(data)
+
+    return data
+
+
+def get_product():
+    data = None
+
+    with open(PRODUCT_CONF_PATH, "r") as f:
+        data = yaml.safe_load(f)
+        logger.debug(data)
+
+    return data
+
+
+def get_order():
+    data = None
+
+    with open(ORDER_CONF_PATH, "r") as f:
+        data = yaml.safe_load(f)
+        logger.debug(data)
+
+    return data
+
+
+def place_order(user):
+    # Perform one time payment, and save order id
+    # on the user model
+    order = {
+        "intent": "CAPTURE",
+        "purchase_units": [
+            {
+                "amount": {
+                    "currency_code": "USD",
+                    "value": "19.95"
+                },
+            }
+        ],
+        "application_context": {
+            "shipping_preference": "NO_SHIPPING",
+            "brand_name": "Video Store Demo"
+        }
+    }
+    ret = myapi.post("v2/checkout/orders", order)
+
+
+def set_paid_until(obj):
+    logger.debug(f"OBJ = {obj}")
+    billing_agreement_id = obj['billing_agreement_id']
+    ret = myapi.get(f"v1/billing/subscriptions/{billing_agreement_id}")
+    logger.debug("===================== Subsciption details =================")
+    logger.debug(ret)
+    if obj['amount']['total'] == "19.99":
+        # hey, but how do I associat it to an user?
+        pass
---
+++ land/payments/stripe.py
@@ -0,0 +1,86 @@
+import paypalrestsdk
+import logging
+import stripe
+from land.models import User
+
+from django.conf import settings
+
+MONTH = 'm'
+ANNUAL = 'a'
+
+API_KEY = settings.STRIPE_SECRET_KEY
+logger = logging.getLogger(__name__)
+
+
+class VideosMonthPlan:
+    def __init__(self):
+        self.stripe_plan_id = settings.STRIPE_PLAN_MONTHLY_ID
+        self.amount = 1995
+
+
+class VideosAnnualPlan:
+    def __init__(self):
+        self.stripe_plan_id = settings.STRIPE_PLAN_ANNUAL_ID
+        self.amount = 19950
+
+
+class VideosPlan:
+    def __init__(self, plan_id):
+        """
+        plan_id is either string 'm' (stands for monthly)
+        or a string letter 'a' (which stands for annual)
+        """
+        if plan_id == MONTH:
+            self.plan = VideosMonthPlan()
+            self.id = MONTH
+        elif plan_id == ANNUAL:
+            self.plan = VideosAnnualPlan()
+            self.id = ANNUAL
+        else:
+            raise ValueError('Invalid plan_id value')
+
+        self.currency = 'usd'
+
+    @property
+    def stripe_plan_id(self):
+        return self.plan.stripe_plan_id
+
+    @property
+    def amount(self):
+        return self.plan.amount
+
+
+def set_paid_until(charge):
+    logger.info(f"set_paid_until with {charge}")
+
+    stripe.api_key = API_KEY
+    pi = stripe.PaymentIntent.retrieve(charge.payment_intent)
+
+    if pi.customer:
+        customer = stripe.Customer.retrieve(pi.customer)
+        email = customer.email
+
+        if customer:
+            subscr = stripe.Subscription.retrieve(
+                customer['subscriptions'].data[0].id
+            )
+            current_period_end = subscr['current_period_end']
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            logger.warning(
+                f"User with email {email} not found"
+            )
+            return False
+
+        user.set_paid_until(current_period_end)
+        logger.info(
+            f"Profile with {current_period_end} saved for user {email}"
+        )
+    else:
+        pass
+        # charge.amount  1990 | 19995
+        # this was one time payment, update
+        # paid_until (e.g. paid_until = current_date + 31 days) using
+        # charge.paid + charge.amount attrs
