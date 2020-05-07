7f063e7d5fe3b4b73b2ec6b2330bfcbfda3f014a
Eugen Ciur <eugen@django-lessons.com>
2020-05-07 08:57:44 +0200

    add basic function definitions

--- land/management/commands/paypal.py
+++ land/management/commands/paypal.py
@@ -27,11 +27,37 @@ class Command(BaseCommand):
             help="List Paypal products or plans"
         )
 
+    def create_product(self):
+        pass
+
+    def create_plan(self):
+        pass
+
+    def list_product(self):
+        pass
+
+    def list_plan(self):
+        pass
+
+    def create(self, what):
+        if what == PRODUCT:
+            self.create_product()
+        else:
+            self.create_plan()
+
+    def list(self, what):
+        if what == PRODUCT:
+            self.list_product()
+        else:
+            self.list_plan()
+
     def handle(self, *args, **options):
         create_what = options.get("create")
         list_what = options.get("list")
 
         if create_what:
             logger.debug(f"Create a {create_what}")
+            self.create(create_what)
         elif list_what:
             logger.debug(f"List {list_what}")
+            self.list(list_what)
